19075fd5f5a162a76bc7b053c2abd43b61104050
IronWhale <ironwhaledev@gmail.com>
2017-11-18 03:39:29 -0500

    more stuff

---
+++ SettleEngine/src/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SettleEngine.src
+{
+    public class GameManager
+    {
+
+
+
+
+        public void Load()
+        { }
+
+        public void Draw()
+        { }
+    }
+}
